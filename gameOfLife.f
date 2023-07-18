8a0d9b8dc3223757d16a25fa14305caf1578a16e
Jose Gustavo Bravo Flores <JGustavoBFlores@gmail.com>
2023-07-18 14:09:43 -0400

    commit

---
+++ gameOfLife.f
@@ -0,0 +1,16 @@
+      PROGRAM GAMEOFLIFE
+      IMPLICIT REAL*8 (A-H,O-Z)
+      LOGICAL, DIMENSION(0:101,0:101) :: old,new
+      
+      old=.FALSE.
+      if(any(old))STOP'what?'
+      DO K=1,10
+      
+      DO J=1,100
+      DO I=1,100
+      nCounter=0
+      END DO
+      END DO      
+
+      END DO
+      END PROGRAM
